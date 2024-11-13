361d1edca621bfb84ee89e7109b1017198d7ea32
booringreader <157519312+booringreader@users.noreply.github.com>
2024-11-13 14:36:22 +0530

    firebase backend

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAiuyrLFd1HWNGqZ2YoxVPAlQgIHBkeLs",
+  authDomain: "media-server-backend-branch.firebaseapp.com",
+  projectId: "media-server-backend-branch",
+  storageBucket: "media-server-backend-branch.firebasestorage.app",
+  messagingSenderId: "935996031597",
+  appId: "1:935996031597:web:09cbe4197d5f50c416b564"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
